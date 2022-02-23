3a133a3f27f2b9cd7798e4520dc148818705243b
leyuri <leyuri97@gmail.com>
2022-02-24 06:56:18 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react'
 import Text from 'components/Text'
 
 const IndexPage: FunctionComponent = function () {
-    return <Text text="yuri lee" />
+    return <Text text="yuri lee :)" />
 }
 
 export default IndexPage
